ade450656ba8b132ace49528c1b10aff7fe82dfb
Ingram <stefanovstefan562@gmail.com>
2026-08-22 20:50:00 +0300

    Format SlotMachineDbContext.cs for consistency

--- SlotMachineDbContext.cs
+++ SlotMachineDbContext.cs
@@ -4,10 +4,10 @@ namespace SlotMachineSimulator
 {
     public class SlotMachineDbContext : DbContext
     {
-        public DbSet<Symbol> Symbols { get; set; }
-        public DbSet<Player> Players { get; set; }
-        public DbSet<Bet> Bets { get; set; }
-        public DbSet<PayoutRule> PayoutRules { get; set; }
+        public DbSet<Symbol> Symbols {get; set;}
+        public DbSet<Player> Players {get; set;}
+        public DbSet<Bet> Bets {get; set;}
+        public DbSet<PayoutRule> PayoutRules {get; set;}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
