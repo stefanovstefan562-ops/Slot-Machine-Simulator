e0c9ade6393434efc7d79a8f7ec8c3f0a1094d04
Ingram <stefanovstefan562@gmail.com>
2026-08-22 20:48:19 +0300

    Refactor SlotMachine class structure

--- SlotMachine.cs
+++ SlotMachine.cs
@@ -2,10 +2,9 @@ namespace SlotMachineSimulator
 {
     public class SlotMachine
     {
-        public int Id { get; set; }
-        public required List<Reel> Reels { get; set; }
-        public required List<PayoutRule> PayoutRules { get; set; }
-
+        public int Id {get; set;}
+        public required List<Reel> Reels {get; set;}
+        public required List<PayoutRule> PayoutRules {get; set;}
         public List<Symbol> SpinAll()
         {
             List<Symbol> results = new List<Symbol>();
@@ -15,7 +14,6 @@ namespace SlotMachineSimulator
             }
             return results;
         }
-
         public Dictionary<Symbol, int> CountSymbols(List<Symbol> results)
         {
             Dictionary<Symbol, int> counts = new Dictionary<Symbol, int>();
@@ -32,7 +30,6 @@ namespace SlotMachineSimulator
             }
             return counts;
         }
-
         public decimal CalculateWinnings(List<Symbol> results)
         {
             Dictionary<Symbol, int> counts = CountSymbols(results);
@@ -45,27 +42,23 @@ namespace SlotMachineSimulator
                     totalWinnings += rule.PayoutAmount;
                 }
             }
-
             return totalWinnings;
         }
-
         public decimal Play(Player player, decimal betAmount)
         {
-
-                if (player.Balance < betAmount)
-            {
-                
-               return 0;
+            if (player.Balance < betAmount)
+            { 
+                return 0;
             }
-                else
+            else
             {
-                player.Balance -= betAmount;
-                List<Symbol> results = SpinAll();
+               player.Balance -= betAmount;
+               List<Symbol> results = SpinAll();
 
-                decimal winnings = CalculateWinnings(results);
-                player.Balance += winnings;
+               decimal winnings = CalculateWinnings(results);
+               player.Balance += winnings;
 
-                return winnings;
+               return winnings;
             }
         }
     }
