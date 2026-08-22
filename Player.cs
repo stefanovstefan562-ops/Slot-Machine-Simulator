b5a36519c7897ba3c1016ebe9b8489d25d436eba
Ingram <stefanovstefan562@gmail.com>
2026-08-22 20:48:47 +0300

    Fix namespace formatting in Player class

--- Player.cs
+++ Player.cs
@@ -2,7 +2,7 @@ namespace SlotMachineSimulator
 {
     public class Player
     {
-        public int Id { get; set; }
+        public int Id {get; set;}
         public required string Name {get; set;}
         public decimal Balance {get; set;}
         public required List<Bet> Bets {get; set;}
